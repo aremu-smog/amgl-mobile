1759ee18a208c5b8fda2b750f994c5c0b0b7a3a5
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-04-15 16:05:45 +0100

    ao_refactor: safearea view implementation

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar"
 import { SafeAreaView, StyleSheet, Text, View } from "react-native"
 import Navigator from "./src/navigation"
 import { AuthContextProvider } from "./src/context/auth.context"
@@ -6,24 +5,14 @@ import { useFonts } from "expo-font"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import QuestionsContextProvider from "./src/context/questions.context"
 
-const statusBarHeight = StatusBar.currentHeight
-
-const IS_ANDROID = Platform.OS === "android"
-const verticalPadding = IS_ANDROID ? statusBarHeight : 0
-
 export default function App() {
-	// useFonts({
-	//   'Graphik': require
-	// })
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
-			<SafeAreaView style={{ flex: 1, marginTop: verticalPadding }}>
-				<AuthContextProvider>
-					<QuestionsContextProvider>
-						<Navigator />
-					</QuestionsContextProvider>
-				</AuthContextProvider>
-			</SafeAreaView>
+			<AuthContextProvider>
+				<QuestionsContextProvider>
+					<Navigator />
+				</QuestionsContextProvider>
+			</AuthContextProvider>
 		</GestureHandlerRootView>
 	)
 }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,14 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native"
 import AppNavigator from "./app.navigator"
 import AuthNavigator from "./auth.navigator"
+import { SafeAreaView, Platform } from "react-native"
+import { StatusBar } from "expo-status-bar"
 import { useAuthContext } from "../context/auth.context"
 
 const Navigator = () => {
 	const { isAuthenticated } = useAuthContext()
-	console.log({ isAuthenticated })
+
+	const statusBarHeight = StatusBar.currentHeight
+	const IS_ANDROID = Platform.OS === "android"
+	const verticalPadding = IS_ANDROID ? statusBarHeight : 0
 	return (
 		<NavigationContainer>
-			{isAuthenticated ? <AppNavigator /> : <AuthNavigator />}
+			{isAuthenticated ? (
+				<SafeAreaView style={{ flex: 1, marginTop: verticalPadding }}>
+					<AppNavigator />
+				</SafeAreaView>
+			) : (
+				<AuthNavigator />
+			)}
 		</NavigationContainer>
 	)
 }

44bee804cba97ad91de2a1c92a0640aacf0c40f7
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-04-27 23:03:33 +0100

    ao_feat: persist authentication

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,7 +1,27 @@
 import "react-native-url-polyfill/auto"
+import * as SecureStore from "expo-secure-store"
 import { createClient } from "@supabase/supabase-js"
 // import { SUPABASE_KEY } from "@env"
 
 const supabaseUrl = "https://yscprznehevulantlliu.supabase.co"
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
-export const supabaseApp = createClient(supabaseUrl, supabaseKey)
+
+const ExpoSecureStoreAdapter = {
+	getItem: key => {
+		return SecureStore.getItemAsync(key)
+	},
+	setItem: (key, value) => {
+		SecureStore.setItemAsync(key, value)
+	},
+	removeItem: key => {
+		SecureStore.deleteItemAsync(key)
+	},
+}
+export const supabaseApp = createClient(supabaseUrl, supabaseKey, {
+	auth: {
+		storage: ExpoSecureStoreAdapter,
+		autoRefreshToken: true,
+		persistSession: true,
+		detectSessionInUrl: false,
+	},
+})

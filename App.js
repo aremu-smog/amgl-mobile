93ee37aae6c71d49795b3da8c095c060d1acd4b9
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-04-27 23:13:56 +0100

    ao_fix: status bar issue on android

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native"
+import { Platform, SafeAreaView, StatusBar } from "react-native"
 import Navigator from "./src/navigation"
 import { AuthContextProvider } from "./src/context/auth.context"
-import { useFonts } from "expo-font"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import QuestionsContextProvider from "./src/context/questions.context"
+const currentHeight = StatusBar.currentHeight
 
+const marginTop = Platform.OS === "android" ? currentHeight : 0
 export default function App() {
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
-			<AuthContextProvider>
-				<QuestionsContextProvider>
-					<Navigator />
-				</QuestionsContextProvider>
-			</AuthContextProvider>
+			<SafeAreaView style={{ flex: 1, marginTop }}>
+				<AuthContextProvider>
+					<QuestionsContextProvider>
+						<Navigator />
+					</QuestionsContextProvider>
+				</AuthContextProvider>
+			</SafeAreaView>
 		</GestureHandlerRootView>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-})

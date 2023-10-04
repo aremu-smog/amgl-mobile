057217ab14b91ff390e408fe67af0841ae04d91c
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-10-04 23:49:56 +0100

    feat: store push notification tokens in db

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ import { AuthContextProvider } from "./src/context/auth.context"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import QuestionsContextProvider from "./src/context/questions.context"
 const currentHeight = StatusBar.currentHeight
-import * as Device from "expo-device"
 import * as Notifications from "expo-notifications"
-import Constants from "expo-constants"
 
 const marginTop = Platform.OS === "android" ? currentHeight : 0
 
@@ -19,52 +17,12 @@ Notifications.setNotificationHandler({
 	}),
 })
 
-async function registerForPushNotificationsAsync() {
-	let token
-	if (Device.isDevice) {
-		const { status: existingStatus } = await Notifications.getPermissionsAsync()
-		let finalStatus = existingStatus
-		if (existingStatus !== "granted") {
-			const { status } = await Notifications.requestPermissionsAsync()
-			finalStatus = status
-		}
-		if (finalStatus !== "granted") {
-			alert("Failed to get push token for push notification!")
-			return
-		}
-
-		token = (
-			await Notifications.getExpoPushTokenAsync({
-				projectId: Constants.easConfig?.projectId,
-			})
-		).data
-	} else {
-		alert("Must use physical device for Push Notifications")
-	}
-
-	if (Platform.OS === "android") {
-		Notifications.setNotificationChannelAsync("default", {
-			name: "default",
-			importance: Notifications.AndroidImportance.MAX,
-			vibrationPattern: [0, 250, 250, 250],
-			lightColor: "#FF231F7C",
-		})
-	}
-
-	return token
-}
-
 export default function App() {
-	const [expoPushToken, setExpoPushToken] = useState("")
 	const [notification, setNotification] = useState(false)
 	const notificationListener = useRef()
 	const responseListener = useRef()
 
 	useEffect(() => {
-		registerForPushNotificationsAsync().then(token => {
-			setExpoPushToken(token)
-		})
-
 		notificationListener.current =
 			Notifications.addNotificationReceivedListener(notification => {
 				setNotification(notification)
--- src/screens/settings/settings.screen.jsx
+++ src/screens/settings/settings.screen.jsx
@@ -1,12 +1,77 @@
 import React, { useState } from "react"
-import { Switch, View, Text } from "react-native"
+import { Switch, View, Text, Alert } from "react-native"
 import { Button } from "../../components"
+import { supabaseApp } from "../../api/supabase"
+import { useAuthContext } from "../../context/auth.context"
+import Constants from "expo-constants"
+import * as Device from "expo-device"
+import * as Notifications from "expo-notifications"
+
+async function registerForPushNotificationsAsync() {
+	let token
+	if (Device.isDevice) {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync()
+		let finalStatus = existingStatus
+		if (existingStatus !== "granted") {
+			const { status } = await Notifications.requestPermissionsAsync()
+			finalStatus = status
+		}
+		if (finalStatus !== "granted") {
+			alert("Failed to get push token for push notification!")
+			return
+		}
+
+		token = (
+			await Notifications.getExpoPushTokenAsync({
+				projectId: Constants.easConfig?.projectId,
+			})
+		).data
+	} else {
+		alert("Must use physical device for Push Notifications")
+	}
+
+	if (Platform.OS === "android") {
+		Notifications.setNotificationChannelAsync("default", {
+			name: "default",
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: "#FF231F7C",
+		})
+	}
+
+	return token
+}
 
 const SettingsScreen = () => {
 	const [isEnabled, setIsEnabled] = useState(false)
+	const { user } = useAuthContext()
 
 	const toggleSwitch = () => {
 		setIsEnabled(prev => !prev)
+	}
+
+	const enablePushNotification = () => {
+		registerForPushNotificationsAsync().then(async token => {
+			try {
+				const { data, error } = await supabaseApp
+					.from("push_notifications")
+					.insert({
+						user_id: user?.id,
+						expo_token: token,
+					})
+					.select()
+
+				if (data) {
+					Alert.alert("Push Notification Enabled")
+				}
+
+				if (error) {
+					console.error(error.message)
+				}
+			} catch (e) {
+				console.error(e.message)
+			}
+		})
 	}
 	return (
 		<View
@@ -29,19 +94,17 @@ const SettingsScreen = () => {
 					}}>
 					<Text style={{ fontSize: 72 }}>😉</Text>
 				</View>
-				<View
-					style={{
-						flexDirection: "row",
-						justifyContent: "space-between",
-						alignItems: "center",
-					}}>
-					<Text>Push notifications:</Text>
-					<Switch
+				<View>
+					<Button
+						text='Enable Push Notifications'
+						onPress={enablePushNotification}
+					/>
+					{/* <Switch
 						trackColor={{ false: "#D6D6D6", true: "#086C72" }}
 						ios_backgroundColor='#3e3e3e'
 						onValueChange={toggleSwitch}
 						value={isEnabled}
-					/>
+					/> */}
 				</View>
 			</View>
 			<LogoutButton />

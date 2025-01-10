512760a789b8843c48c9e5ac3f9b702cf0462d80
Aremu Smog <aremuoluwagbamila@gmail.com>
2025-01-10 23:56:02 +0100

    fix/imports

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { useState, useRef, useEffect } from "react"
-import { Platform, SafeAreaView, StatusBar } from "react-native"
+import {
+	ActivityIndicator,
+	Platform,
+	Text,
+	StatusBar,
+	View,
+	StyleSheet,
+} from "react-native"
 import Navigator from "./src/navigation"
 import { AuthContextProvider } from "./src/context/auth.context"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import QuestionsContextProvider from "./src/context/questions.context"
+import * as Updates from "expo-updates"
 const currentHeight = StatusBar.currentHeight
 import * as Notifications from "expo-notifications"
 
@@ -22,6 +30,22 @@ export default function App() {
 	const notificationListener = useRef()
 	const responseListener = useRef()
 
+	const { currentlyRunning, isUpdateAvailable, isUpdatePending } =
+		Updates.useUpdates()
+
+	const isCurrentlyRunning = !!currentlyRunning
+
+	useEffect(() => {
+		if (isUpdateAvailable) {
+			Updates.checkForUpdateAsync()
+		}
+	}, [isUpdateAvailable])
+
+	useEffect(() => {
+		if (isUpdatePending) {
+			Updates.fetchUpdateAsync()
+		}
+	}, [isUpdatePending])
 	useEffect(() => {
 		notificationListener.current =
 			Notifications.addNotificationReceivedListener(notification => {
@@ -38,6 +62,17 @@ export default function App() {
 			Notifications.removeNotificationSubscription(responseListener.current)
 		}
 	}, [])
+
+	if (isCurrentlyRunning) {
+		return (
+			<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+				<View>
+					<ActivityIndicator size='small' color='#ec1187' />
+					<Text style={style.updateText}>Updating your app</Text>
+				</View>
+			</View>
+		)
+	}
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
 			<AuthContextProvider>
@@ -48,3 +83,9 @@ export default function App() {
 		</GestureHandlerRootView>
 	)
 }
+
+const style = StyleSheet.create({
+	updateText: {
+		marginTop: 16,
+	},
+})

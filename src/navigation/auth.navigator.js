7ee4cf9739eb1aeec0b4b9faf801548cc3f16580
Aremu Smog <aremuoluwagbamila@gmail.com>
2025-01-11 20:17:08 +0100

    feat/auth-navigator

--- src/navigation/auth.navigator.js
+++ src/navigation/auth.navigator.js
@@ -2,7 +2,6 @@ import React from "react"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { LoginScreen, RegisterScreen } from "@/screens"
 import { SCREEN_NAMES } from "@/screens/names"
-import { StatusBar } from "expo-status-bar"
 
 const AuthStack = createNativeStackNavigator()
 
